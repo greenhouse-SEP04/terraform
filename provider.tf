28499d28a56bfa742db178dcf01fa836a04cf7d9
Amin Aouina <amine.aouina.lp.4@gmail.com>
2025-07-26 00:33:21 +0200

    si

--- provider.tf
+++ provider.tf
@@ -26,6 +26,7 @@
 
    # disable AWS-side validation
    skip_credentials_validation = true
+   skip_region_validation      = true
    skip_requesting_account_id  = true
    skip_metadata_api_check     = true
  }
